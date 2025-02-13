b5571d9af4ad7a76b6cbf40d49bc29ef5b73c396
Alex Pushkin <ifc@suz-ppk.ru>
2025-02-13 14:11:00 +1000

    Update

---
+++ RssSample/Controllers/EventsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RssSample.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        NewsService _newsService;
+
+        public EventsController(NewsService newsService)
+        {
+            _newsService = newsService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Events>> Get()
+        {
+            return Ok(_newsService.GetCatNews());
+        }
+    }
+}
---
+++ RssSample/Events.cs
@@ -0,0 +1,14 @@
+﻿namespace RssSample
+{
+    public class Events
+    {
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public DateTime DtStart { get; set; }
+        public DateTime DtEnd { get; set; }
+        public DateTime DtStamp { get; set; }
+        public long Uid { get; set; }
+        public string Location { get; set; }
+        public string Organizer { get; set; }
+    }
+}
